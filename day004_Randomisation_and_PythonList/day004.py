369b2fc02939f5e1e6a0e3a7273fe05b7b628514
lqanh10 <lqa.tdh47@gmail.com>
2023-03-06 19:35:29 +0700

    Create day004.py

---
+++ day004_Randomisation_and_PythonList/day004.py
@@ -0,0 +1,15 @@
+#Randomisation and Python list
+# import random
+
+# random_int = random.randint(10, 100)
+# print(random_int)
+#=========================
+
+import my_module1
+
+
+#print(my_module1.A)
+
+Andy = my_module1.A * 9
+
+print(Andy)
