76f325955c91f33d8634787a52eea86c87997c95
Le Quang Anh (Andy) <lqa.tdh47@gmail.com>
2023-03-06 16:18:48 +0700

    update day4
    update day4

---
+++ day004_Randomisation_and_PythonList/day004_c_LIST.py
@@ -0,0 +1,21 @@
+# example about LIST:
+# fruits = [item1, item2, item3]
+
+states_of_america = ["Kentucky","California", "Texas", "Ohio", "NewJersey", "Alaska", "Utah"]
+
+# print(states_of_america[0])
+# print(states_of_america[-2])
+
+states_of_america[2] = "TESAS"
+
+# print(states_of_america)
+
+states_of_america.append("ENGLAND")
+
+#print(states_of_america)
+
+states_of_america.remove("Alaska")
+#print(states_of_america)
+
+states_of_america.extend(["Berlin", "London", "Paris", "Tel Aviv"])
+print(states_of_america)
---
+++ day004_Randomisation_and_PythonList/day004_d_Exercise 2 - Banker Roulette.py
@@ -0,0 +1,31 @@
+#Exercise 2 - Banker Roulette
+# ou are going to write a program that will select a random name from a list of names. The person selected will have to pay for everybody's food bill.
+
+#Important: You are not allowed to use the choice() function.
+
+#Line 8 splits the string names_string into individual names and puts them inside a List called names. For this to work, you must enter all the names as names followed by comma then space. e.g. name, name, name
+
+
+
+#list1 = "Andy,Henry,Thinh,Cuong,Alvin,Davide,Ray,Alvaro" 
+
+# print(list2)
+
+# for i in range(1):
+#     print(f"{random.choice(list2)} has to pay the bill")
+
+import random
+
+list1 = input("Give me everybody's names, separated by a comma. RANDOM-NAME-HERE is going to buy the meal today! ")
+list2 = list1.split(",")
+
+
+# CACH 1
+# sl = len(list2)
+# ppl = random.randint(0,sl - 1)
+# person_pay_the_bill = list2[ppl]
+
+#CACH 2
+
+person_pay_the_bill = random.choice(list2)
+print(f"{person_pay_the_bill} has to pay the bill")
