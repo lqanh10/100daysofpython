b81d811aaf12335731af2706c1c80dfd9b67d324
Le Quang Anh (Andy) <lqa.tdh47@gmail.com>
2023-03-06 14:26:54 +0700

    update about day 4
    update about day 4

--- day004_Randomisation_and_PythonList/day004.py
+++ day004_Randomisation_and_PythonList/day004.py
@@ -1,15 +1,22 @@
 #Randomisation and Python list
-# import random
+import random
 
 # random_int = random.randint(10, 100)
 # print(random_int)
 #=========================
 
-import my_module1
+#import my_module1
 
 
-#print(my_module1.A)
+# #print(my_module1.A)
 
-Andy = my_module1.A * 9
+# Andy = my_module1.A * 9
 
-print(Andy)
+# print(Andy)
+
+# random_float = random.random() * 5
+# #random_float2 = round(random_float, 3)
+# print(random_float)
+
+love_score = random.randint(1, 100)
+print(f"Your love score is {love_score} point")
---
+++ day004_Randomisation_and_PythonList/day004_b.py
@@ -0,0 +1,2 @@
+#Exercise about heads & tails
+
