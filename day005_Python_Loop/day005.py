c4ab78aef45833c2582bb9456724e208bab54c49
Le Quang Anh (Andy) <lqa.tdh47@gmail.com>
2023-03-07 18:04:58 +0700

    Update day005.py

--- day005_Python_Loop/day005.py
+++ day005_Python_Loop/day005.py
@@ -1,5 +1,8 @@
-fruits = ["Apple", "Peach", "Pear"]
-for fruit in fruits:
-   print(fruit + " Pie")
-print(fruits)
-  
+# fruits = ["Apple", "Peach", "Pear"]
+# for fruit in fruits:
+#    print(fruit + " Pie")
+# print(fruits)
+
+x=9
+type(x)
+print(dir(x))
