03056739fba7f3ce98bc2f270a7ce808204b7ed9
Le Quang Anh (Andy) <lqa.tdh47@gmail.com>
2023-03-27 11:36:48 +0700

    Update day005.py

--- day005_Python_Loop/day005.py
+++ day005_Python_Loop/day005.py
@@ -3,6 +3,16 @@
 #    print(fruit + " Pie")
 # print(fruits)
 
-x=9
-type(x)
-print(dir(x))
+# x=9
+# type(x)
+# print(dir(x))
+
+# print(15+5+(3*2)/4**2+(3-7)*7)
+
+alphabet = 26
+
+letters_in_word = int(input("Enter the number of letters should be in the password : "))
+
+no_of_possibilities = alphabet**letters_in_word
+
+print("No.of possible passwords formed with 6 letters are : ",no_of_possibilities)
